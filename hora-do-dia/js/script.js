615ac1c4b36ebcbf5548437cd0365f77203bd4b2
Lúcio Brito <luciopereirabritoo@gmail.com>
2022-01-28 10:28:38 -0300

    adicionado projeto hora do dia

---
+++ hora-do-dia/js/script.js
@@ -0,0 +1,19 @@
+function carregar (){
+    var msg = document.getElementById('msg');
+    var img = document.getElementById('img');
+    var hora = new Date().getHours();
+    msg.innerHTML = `Agora são ${hora} horas.`;
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.scr = 'img/foto-manha.png';
+        document.body.style.background = '#677AA4'
+    } else if (hora >= 12 && hora <= 18) {
+        //boa tarde
+        img.src = 'img/foto-tarde.png';
+        document.body.style.background = '#FFCD94'
+    } else {
+        //boa noite
+        img.src = 'img/foto-noite.png';
+        document.body.style.background = '#192346'
+    }
+}
